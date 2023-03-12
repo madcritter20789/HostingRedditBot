8d923dc0a505afd1c4f1b2112ffb951edb107293
Chitransh Nishad <91079931+madcritter20789@users.noreply.github.com>
2023-03-12 15:23:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+import time
+import praw
+import random
+
+reddit = praw.Reddit(
+    client_id="Fa5UzGLCvcDg-b5YvFQYUQ",
+    client_secret="iASpu4AM0PNTsMl-jT2KNrmKRa6Kog",
+    password="123asd123asd",
+    user_agent="reddit_bot",
+    username="DependentEcstatic654",
+    check_for_async=False
+)
+
+subreddit = reddit.subreddit("cool")
+print(subreddit.display_name)
+print(subreddit.title)
+print(subreddit.description)
+
+for submission in reddit.subreddit("cars").hot(limit=5):
+    print(submission.title)
+    print(submission.score)
+    print(submission.id)
+    print(submission.url)
+
+for post in reddit.subreddit("cars").hot(limit=5):
+    print(post.title)
+    print(post.score)
+    print(post.id)
+    print(post.url)
+
+
+def karma():
+    try:
+        messages = ["Upvoted, upvote in return","Great Post,cure to upvoteQ"]
+        for submission in reddit.subreddit("FreeKarma4U+FreeKarma4You").stream.submissions():
+            submission.upvote()
+            rand = random.randint(0, (len(messages)-1))
+            print(submission.title)
+            submission.reply(messages[rand])
+            time.sleep(30000)
+    except:
+        time.sleep(30000)
+        karma()
+
+karma()
